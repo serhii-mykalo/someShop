1e38171e948b9d725310d344a65cbc621aa5cb16
Serhii <serhii.mask@gmail.com>
2023-12-25 13:58:12 +0200

    init: start someShop task

---
+++ src/main/java/com/example/SomeShopApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SomeShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SomeShopApplication.class, args);
+	}
+
+}
